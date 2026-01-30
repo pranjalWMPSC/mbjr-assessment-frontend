6b67b23d91cf02a560217f69fa02f69d7abc73c8
PranjalWMPSC <pranjal@wmpsc.in>
2026-01-30 15:57:03 +0530

    links are blocked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+
 import Congrats from './Congrats';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 
 import React from 'react';
 import QRScanner from './QRScanner';
@@ -9,6 +10,7 @@ import Assessment from './Assessment';
 import Certificate from './Certificate';
 import AdminPanel from './AdminPanel';
 import AdminLogin from './AdminLogin';
+
 function App() {
 	const [adminAuthed, setAdminAuthed] = React.useState(() => {
 		return sessionStorage.getItem('adminAuthed') === 'true';
@@ -17,6 +19,17 @@ function App() {
 		setAdminAuthed(true);
 		sessionStorage.setItem('adminAuthed', 'true');
 	};
+
+	// Protect routes: /video, /assessment, /certificate
+	const ProtectedRoute = ({ children }) => {
+		const registered = sessionStorage.getItem('registered') === 'true';
+		const location = useLocation();
+		if (!registered) {
+			return <Navigate to="/" state={{ from: location }} replace />;
+		}
+		return children;
+	};
+
 	return (
 		<Router>
 			<Routes>
@@ -24,9 +37,21 @@ function App() {
 				<Route path="/qr" element={<QRScanner />} />
 				<Route path="/register" element={<RegistrationForm />} />
 				<Route path="/congrats" element={<Congrats />} />
-				<Route path="/video" element={<VideoPlayer />} />
-				<Route path="/assessment" element={<Assessment />} />
-				<Route path="/certificate" element={<Certificate />} />
+				<Route path="/video" element={
+					<ProtectedRoute>
+						<VideoPlayer />
+					</ProtectedRoute>
+				} />
+				<Route path="/assessment" element={
+					<ProtectedRoute>
+						<Assessment />
+					</ProtectedRoute>
+				} />
+				<Route path="/certificate" element={
+					<ProtectedRoute>
+						<Certificate />
+					</ProtectedRoute>
+				} />
 				<Route path="/admin" element={adminAuthed ? <AdminPanel /> : <AdminLogin onLogin={handleAdminLogin} />} />
 			</Routes>
 		</Router>
